1b7011a7ddeeee6e96da5e4e2cfe9e97110d81e9
Aman Mittal <amandeepmittal@live.com>
2018-12-06 18:56:13 +0530

    demo

--- pages/crypto/index.js
+++
@@ -1,5 +0,0 @@
-export default () => (
-	<div>
-		<p>This is a Crypto App!</p>
-	</div>
-);
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,66 @@
+import fetch from 'isomorphic-fetch';
 import Link from 'next/link';
+import Error from 'next/error';
+import Layout from '../components/Layout';
+import StoryList from '../components/StoryList';
 
-export default () => (
-	<div>
-		<h1>👋 Hello from Nextjs </h1>
-		<Link href="/second">
-			<a>Second Page</a>
-		</Link>
-	</div>
-);
+class Index extends React.Component {
+	static async getInitialProps({ req, res, query }) {
+		let stories;
+		let page;
+
+		try {
+			page = Number(query.page) || 1;
+
+			const response = await fetch(
+				`https://node-hnapi.herokuapp.com/news?page=${page}`
+			);
+			stories = await response.json();
+		} catch (err) {
+			console.log(err);
+			// Return Empty Stories when there is an error
+			stories = [];
+		}
+
+		return { page, stories };
+	}
+
+	render() {
+		const { stories, page } = this.props;
+
+		if (stories.length === 0) {
+			return <Error statusCode={503} />;
+		}
+
+		return (
+			<Layout title="Hacker News Clone">
+				<StoryList stories={stories} />
+
+				<button>
+					<Link href={`/?page=${page + 1}`}>
+						<a>Load More</a>
+					</Link>
+				</button>
+
+				<button>
+					<Link href={`/?page=${page - 1}`}>
+						<a>Previous</a>
+					</Link>
+				</button>
+				<style jsx>
+					{`
+						button {
+							font-size: 24px;
+							font-weight: 600;
+						}
+						button a {
+							text-decoration: none;
+						}
+					`}
+				</style>
+			</Layout>
+		);
+	}
+}
+
+export default Index;
